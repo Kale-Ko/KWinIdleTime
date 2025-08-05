3663dcab1c8060b1b4252db827d1fd244726c94d
Kale-Ko <github@kaleko.dev>
2025-08-05 13:51:42 -0400

    Dbus daemon script

---
+++ daemon/main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+
+arg_parser = argparse.ArgumentParser(prog="kwinidletime-daemon", usage="%(prog)s [options]", description="KWin Idle Time Daemon", epilog="For more information, visit https://github.com/Kale-Ko/KWinIdleTime")
+
+arg_parser.add_argument("-t", "--threshold", type=float, default=120.0, help="Set how long it will for the daemon to mark the user as idle (default: 2 minutes)")
+
+args = arg_parser.parse_args(sys.argv[1::])
+
+import asyncio
+import dbus_next
+import dbus_next.aio
+import time
+
+threshold_time: float = args.threshold
+
+running: bool = False
+last_interaction: float = time.time()
+is_idle: bool = False
+
+
+class KWinIdleTime(dbus_next.service.ServiceInterface):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    @dbus_next.service.method(name="MarkInteraction")
+    def MarkInteraction(self):
+        global last_interaction
+        global is_idle
+
+        delta_time: float = time.time() - last_interaction
+        last_interaction = time.time()
+
+        if is_idle:
+            print("User has become active @ {:.0f}ms after {:0.1f} seconds".format(last_interaction * 1000, delta_time))
+
+            is_idle = False
+            self.UserActive(delta_time)
+
+    @dbus_next.service.signal(name="UserIdle")
+    def UserIdle(self) -> "":
+        return
+
+    @dbus_next.service.signal(name="UserActive")
+    def UserActive(self, idle_time: float) -> "d":
+        return idle_time
+
+
+async def run():
+    global threshold_time
+    global running
+    global last_interaction
+    global is_idle
+
+    running = True
+
+    bus: dbus_next.aio.message_bus.MessageBus = dbus_next.aio.message_bus.MessageBus(bus_type=dbus_next.constants.BusType.SESSION)
+
+    await bus.connect()
+
+    await bus.request_name(name="io.github.kale_ko.KWinIdleTime", flags=dbus_next.constants.NameFlag.ALLOW_REPLACEMENT | dbus_next.constants.NameFlag.REPLACE_EXISTING)
+
+    interface: KWinIdleTime = KWinIdleTime(name="io.github.kale_ko.KWinIdleTime")
+
+    bus.export(path="/io/github/kale_ko/KWinIdleTime", interface=interface)
+
+    while running:
+        try:
+            await asyncio.sleep(1)
+        except (KeyboardInterrupt, asyncio.CancelledError):
+            running = False
+
+        if not is_idle:
+            current_time: float = time.time()
+            if current_time - last_interaction > threshold_time:
+                print("User has become idle @ {:.0f}ms".format(last_interaction * 1000))
+
+                is_idle = True
+                interface.UserIdle()
+
+    bus.unexport(path="/io/github/kale_ko/KWinIdleTime", interface=interface)
+
+    await bus.release_name(name="io.github.kale_ko.KWinIdleTime")
+
+    bus.disconnect()
+
+
+try:
+    asyncio.run(run())
+except (KeyboardInterrupt, asyncio.CancelledError):
+    running = False
