3e92750c3ecb719a318e5db0ac468d679283b4f6
Kale-Ko <github@kaleko.dev>
2025-08-14 18:15:02 -0400

    Listen to a few more kwin events

--- kwin_script/contents/code/main.js
+++ kwin_script/contents/code/main.js
@@ -1,7 +1,25 @@
-var i = 0;
+const CURSOR_INTERVAL = 25; // How many cursor events before we fire a MarkInteraction
+
+function markInteraction() {
+    callDBus("io.github.kale_ko.KWinIdleTime", "/io/github/kale_ko/KWinIdleTime", "io.github.kale_ko.KWinIdleTime", "MarkInteraction", function (response) { });
+}
+
+var c = 0;
 workspace.cursorPosChanged.connect(function () {
-    if (i % 25 === 0) {
-        callDBus("io.github.kale_ko.KWinIdleTime", "/io/github/kale_ko/KWinIdleTime", "io.github.kale_ko.KWinIdleTime", "MarkInteraction", function (response) { });
+    if (c % CURSOR_INTERVAL === 0) {
+        markInteraction();
     }
-    i++;
+    c++;
+});
+
+workspace.windowActivated.connect(function (window) {
+    markInteraction();
+});
+
+workspace.currentDesktopChanged.connect(function (desktop) {
+    markInteraction();
+});
+
+workspace.currentActivityChanged.connect(function (activity) {
+    markInteraction();
 });
