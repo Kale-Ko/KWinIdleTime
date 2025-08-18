aa6741e0c800328bc879ce9c5633d2e96c1f84bc
Kale-Ko <github@kaleko.dev>
2025-08-18 12:14:51 -0400

    Update combined script

--- python/combined.py
+++ python/combined.py
@@ -5,8 +5,11 @@ import argparse
 
 arg_parser: argparse.ArgumentParser = argparse.ArgumentParser(prog=sys.argv[0], usage="%(prog)s [options]", description="KWin Idle Time Daemon", epilog="For more information, visit https://github.com/Kale-Ko/KWinIdleTime")
 
-arg_parser.add_argument("-t", "--threshold", type=float, default=120.0, help="Set how long it will for the daemon to mark the user as idle (default: 2 minutes)")
-arg_parser.add_argument("-l", "--listeners-path", type=str, required=True, help="Set the path that listeners are automatically loaded from. Files must be executable to be loaded.")
+arg_parser.add_argument("-t", "--threshold", type=float, default=120.0, help="Set how long it will take for the daemon to mark the user as idle (default: 2 minutes)")
+
+arg_parser.add_argument("-l", "--listeners-path", type=str, required=True, help="Set the path that listeners are automatically loaded from, directory and files must be chmod 0500 to be loaded")
+
+arg_parser.add_argument("--unsafe-allow-permissive-perms", action="store_true", default=False, help=argparse.SUPPRESS)
 
 args: argparse.Namespace = arg_parser.parse_args(sys.argv[1::])
 
@@ -16,72 +19,78 @@ import os
 import os.path
 import subprocess
 
+permissive_perms: bool = args.unsafe_allow_permissive_perms
+
 listeners_path: str = os.path.abspath(args.listeners_path)
 
-listeners: list[str] = []
 
-
-def load_listeners():
-    print(f"Looking for listeners in '{listeners_path}'...")
-
+async def trigger_listeners(args: list[str]):
     os.makedirs(os.path.dirname(listeners_path), exist_ok=True)
 
-    os.umask(0o0277)
     if not os.path.exists(listeners_path):
         os.mkdir(listeners_path, mode=0o0500)
 
-    stat: os.stat_result = os.stat(listeners_path, follow_symlinks=False)
-    if stat.st_uid != os.getuid() or stat.st_gid != os.getgid():
-        raise Exception(f"The owner {stat.st_uid}:{stat.st_gid} of directory '{listeners_path}' is not the current user {os.getuid()}:{os.getgid()}.")
-    elif stat.st_mode & 0o7277 > 0:
-        raise Exception(f"The permissions {oct(stat.st_mode & 0o7777).zfill(3)} of directory '{listeners_path}' are too permissive, set them to 500.")
+    if not permissive_perms:
+        listeners_path_stat: os.stat_result = os.stat(listeners_path, follow_symlinks=False)
+        if listeners_path_stat.st_uid != os.getuid() or listeners_path_stat.st_gid != os.getgid():
+            raise Exception(f"The owner {listeners_path_stat.st_uid}:{listeners_path_stat.st_gid} of directory '{listeners_path}' is not the current user {os.getuid()}:{os.getgid()}")
+        elif listeners_path_stat.st_mode & 0o7277 > 0:
+            raise Exception(f"The permissions {oct(listeners_path_stat.st_mode & 0o7777).zfill(3)} of directory '{listeners_path}' are too permissive, set them to 0500")
 
-    files: list[str] = os.listdir(listeners_path)
+    filenos: list[int] = []
+    tasks: list = []
 
-    for file in files:
-        file: str = os.path.join(listeners_path, file)
-        if not os.path.isfile(file) or not os.access(file, os.R_OK | os.X_OK, follow_symlinks=False):
+    file_paths: list[str] = sorted(os.listdir(listeners_path))
+
+    for file_path in file_paths:
+        file_path: str = os.path.join(listeners_path, file_path)
+
+        if not os.path.isfile(file_path) or not os.access(file_path, os.R_OK | os.X_OK):
             continue
 
-        perms: int = os.stat(file, follow_symlinks=False).st_mode
-        if stat.st_uid != os.getuid() or stat.st_gid != os.getgid():
-            print(f"WARNING: The owner {stat.st_uid}:{stat.st_gid} of file '{file}' is not the current user {os.getuid()}:{os.getgid()}. This listener will not be loaded.", file=sys.stderr)
-            continue
-        elif perms & 0o7277 > 0:
-            print(f"WARNING: The permissions {oct(perms & 0o7777).zfill(3)} of file '{file}' are too permissive, set them to 500. This listener will not be loaded.", file=sys.stderr)
-            continue
+        fileno = os.open(file_path, os.O_RDONLY | os.O_CLOEXEC)
+        filenos.append(fileno)
 
-        listeners.append(file)
+        if not permissive_perms:
+            file_stat: os.stat_result = os.fstat(fileno)
+            if file_stat.st_uid != os.getuid() or file_stat.st_gid != os.getgid():
+                print(f"SKIPPED: The owner {file_stat.st_uid}:{file_stat.st_gid} of file '{file_path}' is not the current user {os.getuid()}:{os.getgid()}", file=sys.stderr)
+                continue
+            elif file_stat.st_mode & 0o7277 > 0:
+                print(f"SKIPPED: The permissions {oct(file_stat.st_mode & 0o7777).zfill(3)} of file '{file_path}' are too permissive, set them to 0500", file=sys.stderr)
+                continue
 
-    if len(listeners) > 0:
-        print(f"Loaded {len(listeners)} listeners successfully.")
-    else:
-        print(f"No listeners found!", file=sys.stderr)
+        exec_task = asyncio.create_subprocess_exec(file_path, *args, executable=f"/proc/self/fd/{fileno}", cwd=listeners_path, start_new_session=True, close_fds=True, pass_fds=[fileno], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        tasks.append(exec_task)
+
+        print(f"Executed {args[0]} listener '{file_path}'")
+
+    try:
+        async with asyncio.timeout(15):
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+
+            print(results)
+    except TimeoutError:
+        print(f"Timeout waiting for listener processes to finish", file=sys.stderr)
+
+        for task in tasks:
+            if not task.done():
+                task.cancel()
+    finally:
+        for fileno in filenos:
+            os.close(fileno)
 
 
 def on_user_idle():
-    for listener in listeners:
-        try:
-            subprocess.run(args=[listener, "idle"], cwd=listeners_path, start_new_session=True, close_fds=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True, timeout=15.0)
-        except subprocess.SubprocessError as e:
-            print(f"Error executing listener '{listener}': {e}", file=sys.stderr)
-            continue
+    print(f"User has become idle")
 
-        print(f"Executed idle listener '{listener}'.")
+    asyncio.create_task(trigger_listeners(args=["idle"]))
 
 
-def on_user_active(delta: float):
-    for listener in listeners:
-        try:
-            subprocess.run(args=[listener, "active", f"{delta:.2f}"], cwd=listeners_path, start_new_session=True, close_fds=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True, timeout=15.0)
-        except subprocess.SubprocessError as e:
-            print(f"Error executing listener '{listener}': {e}", file=sys.stderr)
-            continue
+def on_user_active(delta_time: float):
+    print(f"User has become active after {delta_time:0.1f} seconds")
 
-        print(f"Executed active listener '{listener}'.")
-
-
-load_listeners()
+    asyncio.create_task(trigger_listeners(args=["active", f"{delta_time:0.2f}"]))
 
 
 import asyncio
@@ -137,7 +146,10 @@ async def run(stop_event: asyncio.Event = asyncio.Event()):
 
     await bus.connect()
 
-    await bus.request_name(name="io.github.kale_ko.KWinIdleTime", flags=dbus_next.constants.NameFlag.NONE)
+    requested_name: dbus_next.constants.RequestNameReply = await bus.request_name(name="io.github.kale_ko.KWinIdleTime", flags=dbus_next.constants.NameFlag.DO_NOT_QUEUE)
+    if requested_name != dbus_next.constants.RequestNameReply.PRIMARY_OWNER:
+        print(f"Bus name is already owned, exiting", file=sys.stderr)
+        sys.exit(1)
 
     interface: KWinIdleTime = KWinIdleTime(name="io.github.kale_ko.KWinIdleTime")
 
@@ -145,7 +157,7 @@ async def run(stop_event: asyncio.Event = asyncio.Event()):
 
     while running and not stop_event.is_set():
         try:
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.1)
         except (KeyboardInterrupt, asyncio.CancelledError):
             running = False
 
@@ -161,7 +173,9 @@ async def run(stop_event: asyncio.Event = asyncio.Event()):
 
     bus.unexport(path="/io/github/kale_ko/KWinIdleTime", interface=interface)
 
-    await bus.release_name(name="io.github.kale_ko.KWinIdleTime")
+    release_name: dbus_next.constants.ReleaseNameReply = await bus.release_name(name="io.github.kale_ko.KWinIdleTime")
+    if release_name != dbus_next.constants.ReleaseNameReply.RELEASED:
+        print(f"Failed to release bus name", file=sys.stderr)
 
     bus.disconnect()
 
